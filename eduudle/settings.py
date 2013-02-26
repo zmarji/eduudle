95544dcc8134b6c43880a04a6f9ac36a17536284
zspliff <zspliff@gmail.com>
2013-02-26 01:49:00 -0500

    updated the code to point to system path instead of hard coded path for templates

--- eduudle/settings.py
+++ eduudle/settings.py
@@ -1,5 +1,9 @@
 # Django settings for eduudle project.
 
+import os.path
+
+PROJECT_DIR = os.path.dirname(__file__)
+
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
@@ -110,7 +114,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    "/Users/zmarji/Documents/workspace/eduudle/templates",
+    os.path.join(PROJECT_DIR, "templates"), #locally on "/Users/zmarji/Documents/workspace/eduudle/templates",    
 )
 
 INSTALLED_APPS = (
